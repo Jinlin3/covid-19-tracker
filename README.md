21a07613f99e0016ca8e2b7bdcdcc7ca5cf8fb80
Jack Lin <hellojjlin@gmail.com>
2025-06-11 19:06:22 -0400

    allows a user to select a country and filter data.

---
+++ README.md
@@ -0,0 +1,4 @@
+## To-Do List
+1. Country Selector
+- User selects a single country from all available countries ("location") in the dataset.
+- Display data only for that country going forward.
--- app.py
+++ app.py
@@ -1,5 +1,24 @@
+import pandas as pd
+
 def main():
-  print("main function")
+  # Setup
+  df = pd.read_csv("owid-covid-data.csv")
+  list_of_countries = df["location"].unique().tolist()
+
+  while(True):
+    # User selects a country
+    is_a_country = False
+    print("Please select a country.")
+    while (is_a_country == False):
+      country = input()
+      if (country not in list_of_countries):
+        print("Please try again.")
+      else:
+        is_a_country = True
+
+    print(f'You have selected {country}.')
+    print(f'Here is covid data relating to {country}.')
+    print(df[df["location"] == country].head(n=20))
 
 if __name__ == "__main__":
   main()
