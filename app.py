4569d95c8832a7eab997701d1876747a9e28118f
Jack Lin <hellojjlin@gmail.com>
2025-06-11 20:55:28 -0400

    plotted country specific data with matplotlib

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
   # Setup
@@ -14,11 +15,27 @@ def main():
       if (country not in list_of_countries):
         print("Please try again.")
       else:
-        is_a_country = True
+          is_a_country = True
 
     print(f'You have selected {country}.')
     print(f'Here is covid data relating to {country}.')
-    print(df[df["location"] == country].head(n=20))
+
+    # Selecting specific country
+    country_df = df[df["location"] == country].copy()
+    # Converting date into datetime objects and sorting data by date
+    country_df["date"] = pd.to_datetime(country_df["date"])
+    country_df = country_df.sort_values(by="date")
+
+    # Plot daily new cases
+    plt.figure(figsize=(10,4))
+    plt.plot(country_df["date"], country_df["new_cases"], label="New Cases", color="blue")
+    plt.plot(country_df["date"], country_df["new_deaths"], label="New Deaths", color="red")
+    plt.title(f"Daily COVID-19 Cases and Deaths in {country}")
+    plt.xlabel("Date")
+    plt.ylabel("Count")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
   main()
